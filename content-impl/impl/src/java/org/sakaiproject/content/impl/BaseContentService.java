22365dfe17d3c08824f945581bb8fc19110205b8
ostermmg@whitman.edu <ostermmg@whitman.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-11-09 04:39:04 +0000

    SAK-12126 Failure to remove Collection and swallowed exception yields warning
    svn merge -c38056 https://source.sakaiproject.org/svn/content/trunk/ .
    U    content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
    
    
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/sakai_2-4-x@38057 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
+++ content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -2005,9 +2005,14 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		}
 
 		// check security 
-      if ( ! allowRemoveCollection(edit.getId()) )
+		if ( ! allowRemoveCollection(edit.getId()) )
 		   throw new PermissionException(SessionManager.getCurrentSessionUserId(), 
                                        AUTH_RESOURCE_REMOVE_ANY, edit.getReference());
+
+		// clear thread-local cache SAK-12126
+		ThreadLocalManager.set("members@" + edit.getId(), null);
+		ThreadLocalManager.set("getResources@" + edit.getId(), null);
+		
 
 		// check for members
 		List members = edit.getMemberResources();
@@ -2079,6 +2084,10 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		// get an edit
 		ContentCollectionEdit edit = editCollection(id);
 
+		// clear thread-local cache SAK-12126
+		ThreadLocalManager.set("members@" + edit.getId(), null);
+		ThreadLocalManager.set("getResources@" + edit.getId(), null);
+		
 		// clear of all members (recursive)
 		// Note: may fail if something's in use or not permitted. May result in a partial clear.
 		try
