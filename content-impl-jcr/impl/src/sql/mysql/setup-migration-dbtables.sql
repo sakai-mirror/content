7d7eae75516548699e9adb74fa72ea54db7833c6
sgithens@caret.cam.ac.uk <sgithens@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-11-16 19:12:05 +0000

    SAK-12105 SQL for setting up the table.  The two tables are about to be merged, and the copying is going to be moved into the code.
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/SAK-12105@38249 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ content-impl-jcr/impl/src/sql/mysql/setup-migration-dbtables.sql
@@ -0,0 +1,38 @@
+-- This script is for creating the tables for JCR Content Hosting and JCR 
+-- MailArchive migration.  Each table has the ID's of the things to copy
+-- and a status indicator.
+--
+-- This Script is for MySQL
+
+-- Status codes
+--
+--   0 = Not Started
+--   1 = Completed
+
+
+-- Table and Migration Data for Content Hosting Collections
+DROP TABLE IF EXISTS MIGRATE_JCR_CONTENT_COLLECTION;
+CREATE TABLE MIGRATE_JCR_CONTENT_COLLECTION (
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  COLLECTION_ID varchar(255),
+  STATUS int NOT NULL DEFAULT 0 );
+
+INSERT INTO MIGRATE_JCR_CONTENT_COLLECTION 
+  (COLLECTION_ID)
+  SELECT CONTENT_COLLECTION.COLLECTION_ID
+  FROM CONTENT_COLLECTION;
+
+-- Table and Migration Data for Content Hosting Resources
+DROP TABLE IF EXISTS MIGRATE_JCR_CONTENT_RESOURCE;
+CREATE TABLE MIGRATE_JCR_CONTENT_RESOURCE (
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  RESOURCE_ID varchar(255),
+  RESOURCE_UUID varchar(36),
+  STATUS int NOT NULL DEFAULT 0 );
+
+INSERT INTO MIGRATE_JCR_CONTENT_RESOURCE 
+  (RESOURCE_ID, RESOURCE_UUID)
+  SELECT CONTENT_RESOURCE.RESOURCE_ID, CONTENT_RESOURCE.RESOURCE_UUID
+  FROM CONTENT_RESOURCE;
