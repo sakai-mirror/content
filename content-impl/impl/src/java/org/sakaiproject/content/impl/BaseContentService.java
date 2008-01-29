9f72a5076d575f5f9c4565f1ce82f80311f3e9bb
mmmay@indiana.edu <mmmay@indiana.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-01-29 21:23:42 +0000

    in-143-196:~/java/2-5/sakai_2-5-x/content mmmay$ export EDITOR=picoin-143-196:~/java/2-5/sakai_2-5-x/content mmmay$ svn merge -r 40601:40602  https://source.sakaiproject.org/svn/content/trunk U    content-api/api/src/java/org/sakaiproject/content/api/ContentHostingService.java U    content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java U    content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java in-143-196:~/java/2-5/sakai_2-5-x/content mmmay$ svn log  -r 40601:40602  https://source.sakaiproject.org/svn/content/trunk ------------------------------------------------------------------------ r40602 | jimeng@umich.edu | 2008-01-29 09:55:15 -0500 (Tue, 29 Jan 2008) | 3 lines
    SAK-12671
    Detect attachment id's that are too long and display message to user.
    
    ------------------------------------------------------------------------
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/sakai_2-5-x@40611 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
+++ content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -3412,7 +3412,7 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		{
 			id = id.substring(0, id.length() - 1);
 		}
-
+		
 		// check security
 		checkExplicitLock(id);
 		unlock(AUTH_RESOURCE_ADD, id);
@@ -3713,6 +3713,11 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		String collection = ATTACHMENTS_COLLECTION + IdManager.createUuid() + Entity.SEPARATOR;
 		String id = collection + name;
 
+		if (id.length() > MAXIMUM_RESOURCE_ID_LENGTH)
+		{
+			throw new RuntimeException(ID_LENGTH_EXCEPTION);
+		}
+
 		// add this collection
 		ContentCollectionEdit edit = addCollection(collection);
 		edit.getPropertiesEdit().addProperty(ResourceProperties.PROP_DISPLAY_NAME, name);
@@ -3819,6 +3824,11 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		// form a name based on the attachments collection, a unique folder id, and the given name
 		String collection = toolCollection + IdManager.createUuid() + Entity.SEPARATOR;
 		String id = collection + name;
+		
+		if (id.length() > MAXIMUM_RESOURCE_ID_LENGTH)
+		{
+			throw new RuntimeException(ID_LENGTH_EXCEPTION);
+		}
 
 		// add this collection
 		ContentCollectionEdit edit = addCollection(collection);
