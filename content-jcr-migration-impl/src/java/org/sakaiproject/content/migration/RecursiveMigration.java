d8bb61506b77a090ade68e698c47bb8593fb9eff
sgithens@caret.cam.ac.uk <sgithens@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-05-11 19:53:58 +0000

    SAK-12511 Added in the su to the admin user.  Seems to work fine now.  Next to add the queue db table back in.
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/SAK-12511@46362 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-jcr-migration-impl/src/java/org/sakaiproject/content/migration/RecursiveMigration.java
+++ content-jcr-migration-impl/src/java/org/sakaiproject/content/migration/RecursiveMigration.java
@@ -1,11 +1,15 @@
 package org.sakaiproject.content.migration;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.jcr.LoginException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
+import org.sakaiproject.authz.api.AuthzGroupService;
 import org.sakaiproject.content.api.ContentCollection;
 import org.sakaiproject.content.api.ContentHostingService;
 import org.sakaiproject.content.migration.api.ContentToJCRCopier;
@@ -13,6 +17,10 @@ import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.PermissionException;
 import org.sakaiproject.exception.TypeException;
 import org.sakaiproject.jcr.api.JCRService;
+import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.user.api.UserDirectoryService;
+import org.sakaiproject.user.api.UserNotDefinedException;
 
 /**
  * This is a version of the JCR Migration that just walks through the entire
@@ -28,60 +36,132 @@ import org.sakaiproject.jcr.api.JCRService;
  *
  */
 public class RecursiveMigration {
-	private ContentHostingService contentHostingService;
-	private JCRService jcrService;
-	private ContentToJCRCopier contentToJCRCopier;
+    private ContentHostingService contentHostingService;
+    private JCRService jcrService;
+    private ContentToJCRCopier contentToJCRCopier;
+    private SessionManager sessionManager;
+    private UserDirectoryService userDirectoryService;
+    private AuthzGroupService authzGroupService;
 
-	public void runRecursiveMigration() throws IdUnusedException, TypeException, PermissionException, LoginException, RepositoryException {
-		System.out.println("Running Recursive Migration");
-		System.out.println("+ root");
-		
-		Session jcrSession = jcrService.getSession();
-		
-		ContentCollection root = contentHostingService.getCollection("/");
-		List<String> children = root.getMembers();
-		for (String id: children) {
-			doContentResource(jcrSession, id, 1);
-		}
-	}
-	
-	public void doContentResource(Session session, String path, int depth) throws IdUnusedException, TypeException, PermissionException {
-		StringBuilder strBuilder = new StringBuilder();
-		for (int i = 0; i < depth*2; i++) {
-			strBuilder.append(" ");
-		}
-		
-		if (path.endsWith("/")) {
-			String[] parts = path.split("/");
-			strBuilder.append("+ " + parts[parts.length-1]);
-			contentToJCRCopier.copyCollectionFromCHStoJCR(session, path);
-			ContentCollection collection = contentHostingService.getCollection(path);
-			List<String> children = collection.getMembers();
-			for (String id: children) {
-				doContentResource(session, id, depth+1);
-			}
-		}
-		else {
-			strBuilder.append("- " + path.substring(path.lastIndexOf("/")+1));
-			contentToJCRCopier.copyResourceFromCHStoJCR(session, path);
-		}
-		
-		System.out.println(strBuilder.toString());
-	}
-	
-	/*
-	 * Boilerplate Getters/Setters Below
-	 */
-	public void setContentHostingService(ContentHostingService contentHostingService) {
-		this.contentHostingService = contentHostingService;
-	}
+    private List<String> thingsToMigrate = new ArrayList<String>();
+    private int progress = 0;
+    private Session jcrSession = null;
+    private boolean started = false;
 
-	public void setJcrService(JCRService jcrService) {
-		this.jcrService = jcrService;
-	}
+    public void runRecursiveMigration() throws IdUnusedException, TypeException, PermissionException, LoginException, RepositoryException {
+        System.out.println("Running Recursive Migration");
+        System.out.println("+ root");
 
-	public void setContentToJCRCopier(ContentToJCRCopier contentToJCRCopier) {
-		this.contentToJCRCopier = contentToJCRCopier;
-	}
-	
+        jcrSession = jcrService.getSession();
+
+        ContentCollection root = contentHostingService.getCollection("/");
+        List<String> children = root.getMembers();
+        for (String id: children) {
+            doContentResource(jcrSession, id, 1);
+        }
+
+        // Now that we've recursed and created the list of things to migrate,
+        // let's migrate them.
+        Timer timer = new Timer();
+        progress = 0;
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                if (started == false) { 
+                    becomeAdmin();
+                    started = true;
+                }
+                
+                if (progress < thingsToMigrate.size()) {
+                    String migrateMe = thingsToMigrate.get(progress);
+                    System.out.println("Going to migrate: " + migrateMe);
+                    if (migrateMe.endsWith("/")) {
+                        contentToJCRCopier.copyCollectionFromCHStoJCR(jcrSession, migrateMe);
+                    }
+                    else {
+                        contentToJCRCopier.copyResourceFromCHStoJCR(jcrSession, migrateMe);
+                    }
+                    progress++;
+                }
+                else {
+                    System.out.println("All Done Migrating from the TimerTask");
+                }
+            }
+        };
+
+        timer.schedule(task, 1000, 50);
+    }
+
+    private void becomeAdmin() {
+        User u = null;
+        try {
+            u = userDirectoryService.getUserByEid("admin");
+        } catch (UserNotDefinedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        org.sakaiproject.tool.api.Session s = sessionManager.getCurrentSession();
+
+        s.setUserEid(u.getEid());
+        s.setUserId(u.getId());
+        s.setActive();
+        sessionManager.setCurrentSession(s);
+        authzGroupService.refreshUser(u.getId());
+    }
+
+
+    public void doContentResource(Session session, String path, int depth) throws IdUnusedException, TypeException, PermissionException {
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < depth*2; i++) {
+            strBuilder.append(" ");
+        }
+
+        if (path.endsWith("/")) {
+            String[] parts = path.split("/");
+            strBuilder.append("+ " + parts[parts.length-1]);
+            //contentToJCRCopier.copyCollectionFromCHStoJCR(session, path);
+            thingsToMigrate.add(path);
+            ContentCollection collection = contentHostingService.getCollection(path);
+            List<String> children = collection.getMembers();
+            for (String id: children) {
+                doContentResource(session, id, depth+1);
+            }
+        }
+        else {
+            strBuilder.append("- " + path.substring(path.lastIndexOf("/")+1));
+            //contentToJCRCopier.copyResourceFromCHStoJCR(session, path);
+            thingsToMigrate.add(path);
+        }
+
+        System.out.println(strBuilder.toString());
+    }
+
+    /*
+     * Boilerplate Getters/Setters Below
+     */
+    public void setContentHostingService(ContentHostingService contentHostingService) {
+        this.contentHostingService = contentHostingService;
+    }
+
+    public void setJcrService(JCRService jcrService) {
+        this.jcrService = jcrService;
+    }
+
+    public void setContentToJCRCopier(ContentToJCRCopier contentToJCRCopier) {
+        this.contentToJCRCopier = contentToJCRCopier;
+    }
+
+    public void setSessionManager(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    public void setUserDirectoryService(UserDirectoryService userDirectoryService) {
+        this.userDirectoryService = userDirectoryService;
+    }
+
+    public void setAuthzGroupService(AuthzGroupService authzGroupService) {
+        this.authzGroupService = authzGroupService;
+    }
+
 }
