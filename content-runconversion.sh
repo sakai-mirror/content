5cf10a7d3f6268bdcc3870646e454671f9833e4b
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-02-04 19:36:15 +0000

    NOJIRA update conversion.sh scripts and .classpath
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/sakai_2-5-4@57222 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-runconversion.sh
+++ content-runconversion.sh
@@ -28,16 +28,16 @@ echo HOME:$HOME
 bash ../db/db-util/conversion/runconversion.sh \
 	-j "$m2repository"/commons-collections/commons-collections/3.2/commons-collections-3.2.jar \
 	-j "$m2repository"/commons-pool/commons-pool/1.3/commons-pool-1.3.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-db-api/M2/sakai-db-api-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-component-api/M2/sakai-component-api-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-util/M2/sakai-util-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-util-api/M2/sakai-util-api-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-util-impl/M2/sakai-util-impl-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-entity-api/M2/sakai-entity-api-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-entity-util/M2/sakai-entity-util-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-content-api/M2/sakai-content-api-M2.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-content-impl/M2/sakai-content-impl-M2.jar \
-        -j "$m2repository"/org/sakaiproject/sakai-db-conversion/M2/sakai-db-conversion-M2.jar \
-        -j "$m2repository"/org/sakaiproject/sakai-util-log/M2/sakai-util-log-M2.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-db-api/sakai-2.5.4/sakai-db-api-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-component-api/sakai-2.5.4/sakai-component-api-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-util/sakai-2.5.4/sakai-util-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-util-api/sakai-2.5.4/sakai-util-api-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-util-impl/sakai-2.5.4/sakai-util-impl-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-entity-api/sakai-2.5.4/sakai-entity-api-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-entity-util/sakai-2.5.4/sakai-entity-util-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-content-api/sakai-2.5.4/sakai-content-api-sakai-2.5.4.jar \
+	-j "$m2repository"/org/sakaiproject/sakai-content-impl/sakai-2.5.4/sakai-content-impl-sakai-2.5.4.jar \
+        -j "$m2repository"/org/sakaiproject/sakai-db-conversion/sakai-2.5.4/sakai-db-conversion-sakai-2.5.4.jar \
+        -j "$m2repository"/org/sakaiproject/sakai-util-log/sakai-2.5.4/sakai-util-log-sakai-2.5.4.jar \
         -j "$m2repository"/log4j/log4j/1.2.9/log4j-1.2.9.jar \
 	$@
