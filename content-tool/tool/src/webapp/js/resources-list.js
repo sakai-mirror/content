16b23f02c5d6b861d62ac7393f52ceeb971b82ff
jimeng@umich.edu <jimeng@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-23 11:32:19 +0000

    SAK-1493 Updated scripting in the resources list and enable use of jquery modal dialog.
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/SAK-1493@60978 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-tool/tool/src/webapp/js/resources-list.js
+++ content-tool/tool/src/webapp/js/resources-list.js
@@ -83,3 +83,65 @@ function enableActionMenus()
 	});
 }
 
+/*
+ utilities
+ */
+ var utils = utils || {};
+/*
+ initialize a jQuery-UI dialog
+ */
+utils.startDialog = function (dialogTarget){
+    $("#" + dialogTarget).dialog({
+        close: function(event, ui){
+            utils.resizeFrame('shrink');
+        },
+        autoOpen: false,
+        modal: true,
+        height: 410,
+        maxHeight: 450,
+        width: 500,
+        draggable: true,
+        closeOnEscape: true
+    });
+}
+/*
+ position, open a jQuery-UI dialog, adjust the parent iframe size if any
+ */
+utils.endDialog = function(ev, dialogTarget){
+    var frame;
+    if (top.location !== self.location) {
+        frame = parent.document.getElementById(window.name);
+    }
+    if (frame) {
+        var clientH = document.body.clientHeight + 450;
+        $(frame).height(clientH);
+    }
+	var position = $(ev.target).parents('tr').position();
+	$('.makeMenuChild').hide();
+    
+    $("#" + dialogTarget).dialog('option', 'position', [100, position.top + 10]);
+    $("#" + dialogTarget).dialog("open");
+};
+/*
+ resize the iframe based on the contained document height.
+ used after DOM operations that add or substract to the doc height
+ */
+utils.resizeFrame = function(updown){
+    var clientH;
+    if (top.location !== self.location) {
+        var frame = parent.document.getElementById(window.name);
+    }
+    if (frame) {
+        if (updown === 'shrink') {
+            clientH = document.body.clientHeight;
+        }
+        else {
+            clientH = document.body.clientHeight + 50;
+        }
+        $(frame).height(clientH);
+    }
+    else {
+        // throw( "resizeFrame did not get the frame (using name=" + window.name + ")" );
+    }
+};
+
