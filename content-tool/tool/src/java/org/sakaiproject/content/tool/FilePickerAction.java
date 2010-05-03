28836a5762382306062373c58e6cf9c5c90ba4ac
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-05-03 18:39:05 +0000

    SAK-18282 merge trunk r76534
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/sakai_2-6-x@77025 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java
+++ content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java
@@ -2013,6 +2013,14 @@ public class FilePickerAction extends PagedResourceHelperAction
 	{
 		ToolSession toolSession = SessionManager.getCurrentToolSession();
 		SessionState state = getState(req);
+		
+		Tool tool = ToolManager.getCurrentTool();
+		String url = (String) SessionManager.getCurrentToolSession().getAttribute(tool.getId() + Tool.HELPER_DONE_URL);
+		if (url == null)
+		{
+			cleanup(state);
+			return;
+		}
 
 		if (MODE_ATTACHMENT_DONE.equals(toolSession.getAttribute(STATE_FILEPICKER_MODE)))
 		{
@@ -2037,9 +2045,9 @@ public class FilePickerAction extends PagedResourceHelperAction
 
 			cleanup(state);
 
-			Tool tool = ToolManager.getCurrentTool();
+			//Tool tool = ToolManager.getCurrentTool();
 
-			String url = (String) SessionManager.getCurrentToolSession().getAttribute(tool.getId() + Tool.HELPER_DONE_URL);
+			//String url = (String) SessionManager.getCurrentToolSession().getAttribute(tool.getId() + Tool.HELPER_DONE_URL);
 
 			SessionManager.getCurrentToolSession().removeAttribute(tool.getId() + Tool.HELPER_DONE_URL);
 
