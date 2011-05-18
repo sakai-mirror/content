1a480a7945f3a9c510878573ddfed60e7a4b09d0
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2011-05-18 22:00:22 +0000

    SAK-20280 merge trunk r92643, 92656
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/sakai-2.7.x@93034 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
+++ content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
@@ -3480,6 +3480,15 @@ public class ListItem
 			setNameIsMissing(true);
 			alerts.add(rb.getString("edit.missing"));
 		}
+		
+		Time release = getReleaseDate();
+		Time retract = getRetractDate();
+		if (retract != null && retract.before(release))
+		{
+			alerts.add(rb.getString("edit.retractBeforeRelease"));
+		}
+		
+		
 	    return alerts;
     }
 
