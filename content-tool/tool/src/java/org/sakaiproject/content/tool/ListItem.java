a9f06d7adc9e4d74140f8b8bdbda28e18e5610f9
enietzel@anisakai.com <enietzel@anisakai.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-04-14 18:35:16 +0000

    SAK-23515 merge 308061 to 10.x
    git-svn-id: https://source.sakaiproject.org/svn/content/branches/sakai-10.x@308182 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
+++ content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
@@ -122,6 +122,9 @@ public class ListItem
 	protected static boolean optionalPropertiesEnabled = false;
 
     protected static final Comparator<ContentEntity> PRIORITY_SORT_COMPARATOR = ContentHostingService.newContentHostingComparator(ResourceProperties.PROP_CONTENT_PRIORITY, true);
+    
+    /** Default content type for unknown extensions. */
+    protected static final String UNKNOWN_TYPE = "application/octet-stream";
 
 	public static final String DOT = "_";
 	private static final String PROP_HIDDEN_TRUE = "true"; // SAK-23044
@@ -1655,6 +1658,15 @@ public class ListItem
 		}
 		
 	}
+	
+	//Validates a mimetype change
+	protected void validateMimetype() {
+		//If the mime type matches up with a default unknown type, set it to the unknown type
+		if (ContentTypeImageService.getContentTypeImage(null).equals(ContentTypeImageService.getContentTypeImage(this.mimetype))){
+			//Should get this from the api but it's not in there, just in the impl
+			this.mimetype = UNKNOWN_TYPE;
+		}
+	}
 
 	protected void captureMimetypeChange(ParameterParser params, String index) 
 	{
@@ -1675,7 +1687,7 @@ public class ListItem
 				}
 			}
 		}
-		
+		validateMimetype();
 	}
 
 	protected void captureQuota(ParameterParser params, String index) 
